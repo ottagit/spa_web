8ab8023ae2bf9afa04eb2b8bd1c5700ddf08402d
ottagit <ottacc66@gmail.com>
2018-02-14 16:16:54 +0300

    add zoom, magnify gallery effect to work

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,16 @@
+/* Program */
+
 $(function() {
   new WOW().init();
-})
+});
+
+/* Training sessions */
+$(function() {
+  $("#work").magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+  })
+});
